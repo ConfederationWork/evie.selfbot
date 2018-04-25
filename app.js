679c7b6ec168a31af9aaf9411558fa1ae4c0439c
ConfederationWork <38674591+ConfederationWork@users.noreply.github.com>
2018-04-25 16:56:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,53 +1,74 @@
-const Discord = require("discord.js");
-const client = new Discord.Client({
-    disabledEvents: ['GUILD_BAN_ADD', 'GUILD_BAN_REMOVE', 'CHANNEL_PINS_UPDATE',
-    'USER_NOTE_UPDATE', 'USER_SETTINGS_UPDATE', 'PRESENCE_UPDATE', 'VOICE_STATE_UPDATE',
-    'TYPING_START', 'VOICE_SERVER_UPDATE', 'RELATIONSHIP_ADD', 'RELATIONSHIP_REMOVE']
-});
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const settings = require('./settings.json');
+const chalk = require('chalk');
+const fs = require('fs');
+const moment = require('moment');
+require('./util/eventLoader')(client);
 
-const config = require('./config.json');
-const fs = require("fs");
-const Enmap = require("enmap");
-const EnmapLevel = require("enmap-level");
-
-client.config = config;
-
-require("./modules/functions.js")(client);
-client.db = require("./modules/PersistentDB.js");
-
-client.commands = new Enmap();
-client.aliases = new Enmap();
-client.testing = new Enmap({ provider: new EnmapLevel({ name: 'testing' }) });
-client.mentions = new Enmap({ provider: new EnmapLevel({ name: 'mentions' }) });
-client.myStatus = {
-  lastSpoken: Date.now(),
-  away: false,
-  timeout: 900000
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
 };
 
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
 fs.readdir('./commands/', (err, files) => {
   if (err) console.error(err);
-  console.log(`Loading a total of ${files.length} commands.`);
+  log(`Loading a total of ${files.length} commands.`);
   files.forEach(f => {
-    if(f.split(".").slice(-1)[0] !== "js") return;
-    let props = require(`./commands/${f}`);
+    const props = require(`./commands/${f}`);
+    log(`Loading Command: ${props.help.name}. 👌`);
     client.commands.set(props.help.name, props);
-    if(props.init) props.init(client);
     props.conf.aliases.forEach(alias => {
       client.aliases.set(alias, props.help.name);
     });
   });
 });
 
-fs.readdir('./events/', (err, files) => {
-  if (err) console.error(err);
-  console.log(`Loading a total of ${files.length} events.`);
-  files.forEach(file => {
-    const eventName = file.split(".")[0];
-    const event = require(`./events/${file}`);
-    client.on(eventName, event.bind(null, client));
-    delete require.cache[require.resolve(`./events/${file}`)];
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./commands/${command}`)];
+      const cmd = require(`./commands/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
   });
+};
+
+client.elevation = message => {
+  /* This function should resolve to an ELEVATION level which
+     is then sent to the command handler for verification*/
+  let permlvl = 0;
+  const mod_role = message.guild.roles.find('name', settings.modrolename);
+  if (mod_role && message.member.roles.has(mod_role.id)) permlvl = 2;
+  const admin_role = message.guild.roles.find('name', settings.adminrolename);
+  if (admin_role && message.member.roles.has(admin_role.id)) permlvl = 3;
+  if (message.author.id === settings.ownerid) permlvl = 4;
+  return permlvl;
+};
+
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on('warn', e => {
+  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(config.botToken);
+client.on('error', e => {
+  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+
+client.login(settings.token);
